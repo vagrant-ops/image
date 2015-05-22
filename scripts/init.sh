2473113a32bb31db404ce6de73cfbde723dceb45
fideloper <fideloper@gmail.com>
2015-05-22 14:22:29 -0500

    added locale and swap file setup to base image

--- scripts/init.sh
+++ scripts/init.sh
@@ -9,5 +9,20 @@ sudo add-apt-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
 
+# Charset/Locale
+sudo locale-gen C.UTF-8
+export LANG=C.UTF-8
+echo "export LANG=C.UTF-8" >> /home/vagrant/.bashrc
+
+# Swap
+sudo fallocate -l 512M /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo "/swapfile   none    swap    sw    0   0" | sudo tee -a /etc/fstab
+printf "vm.swappiness=10\nvm.vfs_cache_pressure=50" | sudo tee -a /etc/sysctl.conf && sysctl -p
+
+# Ansible.cfg file?
+
 # Add some Python requirements
 ## sudo pip install pyyaml
